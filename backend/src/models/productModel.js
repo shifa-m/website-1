9c60a721adfb93fc3d975f45386b84b8b7a8d5d3
shifa-m <shifamahreen786@gmail.com>
2026-05-28 23:25:36 +0530

    routes

--- backend/src/models/productModel.js
+++ backend/src/models/productModel.js
@@ -45,6 +45,39 @@ const productSchema=mongoose.Schema({
             category:{
                         type:ObjectId,
                         ref:"Category",
-                        required:true
-            }
-})
+                        required:true,
+            },
+            description:{
+                        type:String,
+                        required:true,
+            },
+            reviews:
+            [reviewSchema],
+            rating:{
+                        type:Number,
+                        required:true,
+                        default:0
+            },
+            numReviews:{
+                        type:Number,
+                        required:true,
+                        default:0
+            },
+            price:{
+                        type:Number,
+                        required:true,
+                        default:0
+            },
+            countInStock:{
+                        type:Number,
+                        required:true,
+                        default:0
+            },
+            
+},
+{timestamps:true}
+
+);
+
+const Product=mongoose.model("Product",productSchema);
+export default Product;
--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -5,3 +5,49 @@ import multer from "multer";
 
 const router=express.Router();
 
+const storage=multer.diskStorage({
+            destination:(req,file,cb)=>{
+                        cb(null,"uploads/")
+            },
+
+            filename:(req,file,cb)=>{
+                        const extname=path.extname(file.originalname);
+                        cb(null,`${file.fieldname}-${Date.noew()}${extname}`);
+            },
+});
+
+const fileFilter=(req,res,cb)=>{
+
+            const filetypes = /jpe?g|png|webp/;
+            const mimetypes = /images\/jpe?g|image\/png|image\/webp/;
+
+            const extname=path.extname(file.originalname).toLowerCase();
+            const mimetype=file.mimetype;
+
+            if(filetypes.test(extname) && mimetypes.test(mimetype)){
+                        cb(null,true);
+            }else{
+                        cb(new Error("Images Only "),false);
+            }
+            };
+
+const upload=multer({storage,fileFilter});
+const uploadSingleImage=upload.single("image");
+
+router.post("/",(req,res)=>{
+            uploadSingleImage(req,res,(err)=>{
+                        if(err){
+                                    res.status(400).send({message:err.message});
+                        }else if(req.file){
+                                    res.status(200).send({
+                                                message:"Image uploaded successfully",
+                                                image:`/${req.file.path}`;
+                                    });
+                        }else{
+                                    res.status(400).send({message:"No image file provided"});
+                        }
+                        }
+            )
+});
+
+export default router;
