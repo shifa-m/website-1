0e4a8bbc22db3180f56668d3d4e7a49f0e9192b4
shifa-m <shifamahreen786@gmail.com>
2026-05-31 17:02:34 +0530

    Order & category router

--- backend/src/controllers/categoryController.js
+++ backend/src/controllers/categoryController.js
@@ -21,4 +21,68 @@ const createCategory=asyncHandler(async(req ,res)=>{
                                     })
                         }
             }
+});
+
+const updateCategory=asyncHandler(async(req,res)=>{
+
+            try{
+                        const {name}=req.body;
+                        const {categoryId}=req.params;
+
+                        const category=await Category.findOne({_id:categoryId});
+
+                        if(!category){
+                                    res.status(500).json({
+                                                error:"Category not found"
+                                    });
+                        }
+
+                        category.name=name;
+
+                        const updatedCategory=await category.save();
+                        res.json(updatedCategory);
+            }catch(error){
+                        console.log(error);
+                        res.status(500).json({error:"Internal server error"});
+            }
+});
+
+const removeCategory=asyncHandler(async(req,res)=>{
+
+            try{
+                        const removed=await Category.findByIdAndRemove(req.params.categoryId);
+                        res.json(removed);
+            }catch(error){
+                        console.log(error);
+                        res.status(500).json({error:"Internal server error"})
+            }
 })
+
+const listCategory=asyncHandler(async(req,res)=>{
+            try{
+                        const all=await Category.find({});
+                        res.json(all);
+            }catch(error){
+                        console.log(error);
+                        return res.status(400).json(error.message);
+            }
+})
+
+const readCategory=asyncHandler(async(req,res)=>{
+            try{
+                        const category=await Category.findOne({_id:req.params.id});
+                        res.json(category)
+            }catch(error){
+                        console.log(error);
+                        return res.status(400).json(error.message)
+            }
+
+});
+
+export{
+            createCategory,
+            updateCategory,
+            removeCategory,
+            listCategory,
+            readCategory
+}
--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -32,5 +32,197 @@ const createOrder=async(req,res)=>{
                                     res.status(400);
                                     throw new Error("No Order items");
                         }
+
+
+                        const itemsFromDB=await Product.find({
+                                    _id:{$in:orderItems.map((x)=>x._id)},
+                        });
+
+
+                        const dbOrderItems=orderItems.map((itemFromClient)=>{
+                                    const matchingItemFromDB=itemsFromDB.find(
+                                                (itemFromDB)=>itemFromDB._id.toString()===itemFromClient._id
+                                    );
+
+                        if(!matchingItemFromDB){
+                                    res.status(400);
+                                    throw new Error(`Product not found : ${itemFromClient._id}`)
+                        }
+
+                        return{
+                                    ...itemFromClient,
+                                    product:itemFromClient._id,
+                                    price:matchingItemFromDB.price,
+                                    _id:undefined,
+                        }
+                        });
+
+                        const {itemsPrice,taxPrice,shippingPrice,totalPrice}=
+                        calcPrices(dbOrderItems);
+
+
+                        const order=new Order({
+                                  orderItems:dbOrderItems,
+                                  user:req.user._id,
+                                  shippingAddress,
+                                  paymentMethod,
+                                  itemsPrice,
+                                  taxPrice,
+                                  shippingPrice,
+                                  totalPrice,  
+                        });
+
+
+                        const createdOrder=await order.save();
+                        res.status(201).json(createOrder);
+            }catch(errror){
+                        res.status(500).json({error:error.message})
             }
+};
+
+
+const getAllOrders=async(req,res)=>{
+            try{
+                        const orders=await Order.find({}).populate("user","id username");
+                        res.json(orders)
+            }catch(error){
+                        res.status(500).json({error:error.message});
+            }
+};
+
+const getUserOrders=async(req,res)=>{
+            try{
+                        const orders=await Order.find({user:req.user._id});
+                        res.json(orders);
+            }catch(error){
+                        res.status(500).json({
+                                    error:error.message
+                        })
+            }
+}
+
+const countTotalOrders=async(req,res)=>{
+            try{
+                        const totalOrders=await Order.countDocumnets();
+                        res.json({totalOrders})
+            }catch(error){
+                        res.status(500).json({
+                                    error:error.message
+                        })
+            }
+}
+
+const calculateTotalSales=async (req,res)=>{
+            try{
+                        const orders=await Order.find();
+                        const totalSales=orders.reduce((sum,order)=>sum+order.totalPrice,0);
+                        res.json({totalSales})
+            }catch(error){
+                        res.status(500).json({
+                                    error:error.message
+                        })
+            }
+}
+
+
+const calculateTotalSalesByDate=async (req,res)=>{
+            try{
+                        const salesByData=await Order.aggregrate([{
+                                    $match:{
+                                                isPaid:true,
+                                    }
+                        },
+            {
+                        $group:{
+                                    _id:{
+                                                $dateToString:{format:"%Y-%m-%d",date:"$paidAt"},
+                                    },
+                                    totalSales:{$sum:"$totalPrice"},
+                        }
+            }
+]);
+
+res.json(salesByData);
+            }catch(error){
+                        res.status(500).json({error:error.message})
+            }
+};
+
+const findOrderById=async(req,res)=>{
+            try{
+                        const order=await Order.findById(req.params.id).populate(
+                                    "user",
+                                    "username email"
+                        );
+
+                        if(order){
+                                    res.json(order);
+                        }else{
+                                    res.status(404);
+                                    throw new Error("Order not found")
+                        }
+            }catch(error){
+                        res.status(500).json({
+                                    error:error.message
+                        })
+            }
+};
+
+const markOrderAsPaid=async(req,res)=>{
+            try{
+                        const order=await order.findById(req.params.id);
+
+                        if (order){
+                                    order.isPaid=true;
+                                    order.paidAt=Date.now();
+                                    order.paymentResult={
+                                                id:req.body.id,
+                                                status:req.body.status,
+                                                update_time:req.body.update_time,
+                                                email_address:req.body.payer.email_address,
+                                    };
+
+
+                                 const updateOrder=await order.save();
+                                 res.status(200).json(updateOrder);   
+                        }else{
+                                    res.status(404);
+                                    throw new Error("Order not found")
+                        }
+            }catch(error){
+                        res.status(500).json({error:error.message})
+            }
+}
+
+
+const markOrderAsDelivered=async(req,res)=>{
+            try{
+                        const order=await Order.findById(req.params.id);
+
+                        if(order){
+                                    order.isDelivered=true;
+                                    order.deliveredAt=Date.now();
+
+                                    const updateOrder=await order.save();
+                                    res.json(updatedOrder);
+                        }else{
+                                    res.status(404);
+                                    throw new Error("Order not found");
+                        }
+            }catch(error){
+                        res.status(500).json({error:error.message});
+            }
+}
+
+
+export{
+            createOrder,
+            getAllOrders,
+            getUserOrders,
+            countTotalOrders,
+            calculateTotalSales,
+            calculateTotalSalesByDate,
+            findOrderById,
+            markOrderAsPaid,
+            markOrderAsDelivered
 }
