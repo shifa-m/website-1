21a0a83af22fcf487e807f5c00db44d13f91a6a2
shifa-m <shifamahreen786@gmail.com>
2026-05-31 22:13:13 +0530

    middleware

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js"
+import asyncHandler from "./asyncHandler.js";
+
+
+const authenticate=asyncHandler(async(req,resizeBy,next)=>{
+
+            let token;
+
+            if(token){
+                        try{
+                                    const decoded=jwt.verify(token,process.env.JWT_SECRET);
+                                    req.user=await User.findById(decoded.userId).select("-password")
+                                    next();
+                        }catch(error){
+                                    res.status(401);
+                                    throw new Error("Not Authorized,token failed")
+                        }
+            }else{
+                        res.status(401);
+                        throw new Error("Not Authorized,no token");
+            }
+
+})
+
+
+const authorizeAdmin=(req,res,next)=>{
+            if(req.user && req.user.isAdmin){
+                        next();
+            }else{
+                        res.status(401).send("Not authorized as an admin")
+            }
+}
+
+
+export {authenticate,authorizeAdmin}
